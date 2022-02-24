faa232e526971af02638dfb8ae1b4b0f0d9404bc
Andreas Boräng <andbor02@gmail.com>
2022-02-24 15:24:55 +0100

    Added css to nav. Added responsiveness

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,10 +1,12 @@
 window.onscroll = function() {sticky_nav()};
-const header = document.getElementById("nav");
+const nav = document.getElementById("nav");
 const sticky = nav.offsetTop;
 
+
 function sticky_nav() {
-    console.log("y");
-    if (window.pageYOffset > sticky) {
+    let width = window.innerWidth;
+
+    if (window.pageYOffset > sticky && width >= 700) {
       nav.classList.add("nav-sticky");
     } else {
       nav.classList.remove("nav-sticky");
@@ -16,3 +18,43 @@ function sticky_nav() {
   const menu_icon = document.getElementById("menu-icon");
   const menu_close = document.getElementById("menu-close");
 
+menu_icon.addEventListener("click", () => {
+    menu_icon.classList.remove("d-inline-block");
+    menu_icon.classList.add("d-none");
+    menu_close.classList.remove("d-none");
+    menu_close.classList.add("d-inline-block");
+    nav.classList.toggle("active");
+});
+
+menu_close.addEventListener("click", () => {
+    menu_icon.classList.add("d-inline-block");
+    menu_icon.classList.remove("d-none");
+    menu_close.classList.add("d-none");
+    menu_close.classList.remove("d-inline-block");
+    nav.classList.toggle("active");
+});
+
+window.onscroll = () => {
+    menu_icon.classList.add("d-inline-block");
+    menu_icon.classList.remove("d-none");
+    menu_close.classList.add("d-none");
+    menu_close.classList.remove("d-inline-block");
+    nav.classList.remove("active");
+};
+
+window.onresize = remove_icon;
+
+function remove_icon(){
+    const width = document.body.clientWidth;
+
+    if(width >= 700){
+        menu_icon.classList.remove("d-inline-block");
+        menu_close.classList.remove("d-inline-block");
+        menu_icon.classList.add("d-none");
+        menu_close.classList.add("d-none");
+    }
+    else{
+        menu_icon.classList.add("d-inline-block");
+        menu_icon.classList.remove("d-none");
+    }
+}
